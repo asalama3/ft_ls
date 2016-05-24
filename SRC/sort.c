7a2a876e9f7ea21063b42f4f789c93e593b26301
Andrea SALAMA <asalama@e2r4p6.42.fr>
2016-05-24 12:56:50 +0200

    list

--- SRC/sort.c
+++ SRC/sort.c
@@ -1,68 +1,58 @@
-// fonction sort alpha
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asalama <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/05/24 10:50:38 by asalama           #+#    #+#             */
+/*   Updated: 2016/05/24 12:56:23 by asalama          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+//fonction sort alpha
 // fonction sort time
 // fonction sort files and dir
 // fonction sort reverse
 
 #include "ft_ls.h"
 
-void			arg_sort_alpha(t_arg **arg_lst, t_arg *new)
+void			arg_sort_alpha(t_arg **arg_lst)
 {
 	t_arg	*runner;
+	
+
+}
+
+/*void			arg_sort_alpha(t_arg **arg_lst, t_arg *new)
+{
+	t_arg	*runner;
+	t_arg	*next_runner;
 
 	runner = *arg_lst;
+	next_runner = runner->next;
 
 	while (runner->next != NULL)
 	{
-		if (ft_strcmp(runner->name, new->name) < 0)
-		{	
-			ft_putendl(runner->name);
-			runner = runner->next;
+		if (ft_strcmp(runner->name, next_runner->name) > 0)
+		{
+			next_runner->prev = runner->prev;
+			runner->prev = next_runner;
+			runner->next = next_runner->next;
+			next_runner->next = runner;
+			next_runner = runner->next;
 		}
 		else
 		{
-			ft_putendl("NOT SORTED");
-			push_back(runner, new);
-//			if (runner->prev)
-//			{
-//				runner->prev->next = runner->next;
-//			}
-//			if (runner->next)
-//			{
-//				runner->next->prev = runner->prev;
-//			}
-//			runner->next = link;
-//			link->prev = runner;
 			runner = runner->next;
+			next_runner = runner->next;
 		}
-//		runner = *arg_lst;
 	}
+	print_arg_list(arg_lst);
 }
-
-
-/*	while (runner->next != NULL && ft_strcmp(runner->name, link->name) < 0)
-	{
-//		ft_putendl(runner->name);
-		runner = runner->next;
-	}
-	if (runner->next != NULL && ft_strcmp(runner->name, link->name) > 0)
-	{
-		ft_putendl(runner->name);
-		push_front(arg_lst, link);
-	}
-
-
-//	if (runner->prev == NULL)
-//		push_front(arg_lst, link);
-
-	{
-		ft_putendl("SORT ALPHA");
-		if (ft_strcmp(link->name, runner->name) > 0)
-		{
-			push_back(runner, link);
-		}
-		runner = runner->next;
-	}
 */
+
 
 void			arg_sort_time(t_arg **arg_lst, t_arg *link)
 {
