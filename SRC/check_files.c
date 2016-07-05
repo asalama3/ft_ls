04027109da01a54b51254feddd4afa032891683d
JEREMY MARUANI <jeremymaruani@iMac-de-JEREMY.local>
2016-07-05 15:46:23 +0200

    new files for norminette and renamed

---
+++ SRC/check_files.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_files.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asalama <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/06/07 12:38:05 by asalama           #+#    #+#             */
+/*   Updated: 2016/07/05 15:27:09 by asalama          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_ls.h"
+
+void		option_l(t_arg *runner, t_file *file)
+{
+	l_info(runner, file);
+	print_l_info(file);
+}
+
+void		dir_ok(t_arg *runner, t_arg *dir_lst, t_flags *option, t_file *file)
+{
+	if ((runner->dir = opendir(runner->path)))
+		make_dir(dir_lst, runner, option, file);
+	if (closedir(runner->dir) == -1)
+		error_exit("Error Malloc", EXIT_FAILURE);
+}
+
+void			test_dir(t_arg **old_lst, t_flags *option, t_arg *error_lst)
+{
+	t_arg	*runner;
+	t_arg	*dir_lst;
+	t_file	*file;
+
+	runner = *old_lst;
+	dir_lst = NULL;
+	if (!(file = (t_file*)ft_memalloc(sizeof(t_file))))
+		error_exit("Error malloc", EXIT_FAILURE);
+	while (runner != NULL)
+	{
+		if (option->l && !S_ISDIR(runner->buf->st_mode))
+		{
+			option_l(runner, file);
+//			l_info(runner, file);
+//			print_l_info(file);
+		}
+		if (((list_len(old_lst) > 1 || error_lst)) && (S_ISDIR(runner->buf->st_mode)))
+			print_file(runner, option);
+		if (S_ISDIR(runner->buf->st_mode))
+		{
+			dir_ok(runner, dir_lst, option, file);
+//			if ((runner->dir = opendir(runner->path)))
+//				make_dir(dir_lst, runner, option, file);
+//			if (closedir(runner->dir) == -1)
+//				error_exit("Error Malloc", EXIT_FAILURE);
+			runner = runner->next;
+		}
+		else
+			runner = runner->next;
+	}
+}
+
+void			test_file(t_arg **old_lst)
+{
+	t_arg	*runner;
+	t_arg	*dir_lst;
+
+	runner = *old_lst;
+	dir_lst = NULL;
+	while (runner != NULL)
+	{
+		if (!S_ISDIR(runner->buf->st_mode) && !S_ISLNK(runner->buf->st_mode))
+		{
+			ft_putstr(runner->name);
+			ft_putstr("\n");
+		}
+		runner = runner->next;
+	}
+}
---
+++ SRC/print_error.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_error.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asalama <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/03/24 12:38:06 by asalama           #+#    #+#             */
+/*   Updated: 2016/07/05 12:14:40 by asalama          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_ls.h"
+
+void		error_exit(char *str, int error)
+{
+	perror(str);
+	exit(error);
+}
+
+void		ft_stat_error(char *file)
+{
+	ft_putstr_fd("ls: ", 2);
+	ft_putstr_fd(file, 2);
+	ft_putstr_fd(" :", 2);
+	ft_putendl_fd(strerror(errno), 2);
+}
+
+void		ft_error(char option)
+{
+	ft_putstr_fd("ls: illegal option -- ", 2);
+	ft_putchar_fd(option, 2);
+	ft_putchar_fd('\n', 2);
+	ft_putendl_fd("usage: ls [-ABCFGHLOPRSTUWabcdefghiklmnopqrstuwx1] [file ...]" , 2);
+	exit(EXIT_FAILURE);
+}
